17bafbf7e8a0cf1d8a8357627cfb188b5f218c6f
yesiddev <yesiddeveloper@gmail.com>
2022-11-03 10:33:41 -0500

    refactoring

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,15 +1,12 @@
 export const fizzbuzz = (num) => {
-  if (typeof num !== 'number') {
-    throw new Error('parameter is not a number')
-  }
-
   if (num % 3 === 0 && num % 5 === 0) {
     return 'FizzBuzz'
-  } else if (num % 3 === 0) {
-    return 'Fizz'
-  } else if (num % 5 === 0) {
-    return 'Buzz'
-  } else {
-    return num
   }
+  if (num % 3 === 0) {
+    return 'Fizz'
+  }
+  if (num % 5 === 0) {
+    return 'Buzz'
+  }
+  return num
 }
