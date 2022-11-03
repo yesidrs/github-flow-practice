a7773cdbab499bc090b5ae2ec8143c237ef295c5
yesiddev <yesiddeveloper@gmail.com>
2022-11-03 08:31:20 -0500

    refactoring

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,11 +1,12 @@
 export const fizzbuzz = (num) => {
   if (num % 3 === 0 && num % 5 === 0) {
     return 'FizzBuzz'
-  } else if (num % 3 === 0) {
-    return 'Fizz'
-  } else if (num % 5 === 0) {
-    return 'Buzz'
-  } else {
-    return num
   }
+  if (num % 3 === 0) {
+    return 'Fizz'
+  }
+  if (num % 5 === 0) {
+    return 'Buzz'
+  }
+  return num
 }
